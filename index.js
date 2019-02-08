e72415674f0779fbc498c26e879d540738c04339
Mikhail Lapshin <lapshin.michail@gmail.com>
2019-02-08 11:31:43 +0300

    add headers for CORS

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let app = express();
 
 function addCORS(res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     return res;
 }
 
